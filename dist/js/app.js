449af80ff504f7b51f1c54a56f1a5a467c7387ba
devByBjorn <devbybjorn@gmail.com>
2020-01-17 10:26:51 +0100

    alert modal message

--- dist/js/app.js
+++ dist/js/app.js
@@ -68,6 +68,11 @@ const hideOrDisplayByClass = (className, display) => {
   element.style.display = display
 }
 
+// Alert modal close button
+document.querySelector('.close-btn').addEventListener('click', () => {
+  hideOrDisplayByClass('.alert-modal', 'none')
+})
+
 // Desktop keypress guess - if other charachter than a-z or dash('-') - display alert box. 
 window.addEventListener('keypress', (e) => {
   // const specialChar = [229, 228, 246]
@@ -75,12 +80,12 @@ window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game.getGuess(guess)
     lightUpKey(e)
-    hideOrDisplayByClass('.alert-box', 'none')
+    hideOrDisplayByClass('.alert-modal', 'none')
   } else if (e.charCode === 13) {
     resetLetters()
     getGame()
   } else {
-    hideOrDisplayByClass('.alert-box', 'inline-block')
+    hideOrDisplayByClass('.alert-modal', 'inline-block')
   }
 
   render()
@@ -103,7 +108,7 @@ document.querySelector('.reset-button').addEventListener('click', () => {
   resetLetters()
 
   // If alertbox is displaying - hide
-  hideOrDisplayByClass('.alert-box', 'none')
+  hideOrDisplayByClass('.alert-modal', 'none')
 
   // Get new game
   getGame()
@@ -114,7 +119,7 @@ document.querySelectorAll('.letter').forEach((letter) => {
   letter.addEventListener('click', (e) => {
     const guess = e.target.id
     game.getGuess(guess)
-    hideOrDisplayByClass('.alert-box', 'none')
+    hideOrDisplayByClass('.alert-modal', 'none')
 
     // Makes sure gussed letters stay hidden if light swith is used
     if (localStorage.getItem('theme') === 'theme-dark') {
